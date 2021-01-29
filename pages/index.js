f3e185c3a9492fd12e1901854a4b724cdba5ad82
g <gustavosasakironcaglia@usp.br>
2021-01-29 00:01:57 -0300

    primeira aula react

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,32 @@
 import styled from 'styled-components'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import Widget from '../src/components/Widget'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizContainer from '../src/components/QuizContainer'
+import db from '../style_db.json'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return(
+  <QuizBackground backgroundImage={db.bg}>
+
+    <QuizContainer>
+      <Widget>
+        <Widget.Header>
+          <h1>{db.title}</h1>
+        </Widget.Header>
+
+        <Widget.Content>
+          <h1>{db.title}</h1>
+        </Widget.Content>
+      </Widget>
+    </QuizContainer>
+
+    <GitHubCorner projectUrl="https://www.alura.com.br/imersao-react-next-js/aula01-react-nextjs-aluraquiz?utm_source=ActiveCampaign&utm_medium=email&utm_content=Imers%C3%A3o+React+Next+js%3A+Primeira+aula+no+ar%21&utm_campaign=%5BIMERS%C3%83O+REACT+2+0%5D+%28Aula%29+01"/>
+    <Footer />
+  </QuizBackground>
+  )
 }
